478779168bf297a52dc7815286df3fc968df3768
Gaurav Singh Bisht <gauravbisht712530@gmail.com>
2024-09-09 19:53:22 +0530

    version 2

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import styles from "./App.module.css";
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
+import { useState } from "react";
+function App() {
+  const [calVal, setCalVal] = useState("");
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
+  return (
+    <div className={styles.calculator}>
+      <Display displayValue={calVal} />
+      <ButtonsContainer onButtonClick={onButtonClick} />
+    </div>
+  );
+}
+
+export default App;
